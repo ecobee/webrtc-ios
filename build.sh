967adc59d96b91414a5062b491ad82751c878690
Vincent Polsinelli <vincent.polsinelli@gmail.com>
2020-04-21 17:38:11 -0400

    Updated build script and created GitHub action

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
+source config.sh
+
 usage() { echo "Usage: $0 [-b BRANCH] [-v VERSION]" 1>&2; exit 1; }
 
 while getopts ":b:v:" arg; do
@@ -21,18 +26,29 @@ if [ -z "${branch}" ] || [ -z "${version}" ]; then
     usage
 fi
 
+# Setup environment
+root_dir=$(pwd)
+build_dir="${root_dir}/build/"
+mkdir $build_dir
+cd "${build_dir}"
+
 # Setup tooling
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-current_dir=$(pwd)
-tools_dir="${current_dir}/depot_tools/"
+tools_dir="${build_dir}/depot_tools/"
 export PATH=$PATH:$tools_dir
 
 # Fetch repo and switch to release
 fetch --nohooks webrtc_ios
 gclient sync
-cd src
+cd "${build_dir}/src"
 git checkout -b "${branch}"
 
-# make the framework file, may need xcode installed
-cd src/tools_webrtc/ios/
-python build_ios_libs.py --bitcode --revision "${version}"
+# make the framework file
+cd "${build_dir}/src"
+gn gen out/ios --args='target_os="ios" ios_enable_code_signing=false target_cpu="arm64"' --ide=xcode
+cd "${build_dir}/src/tools_webrtc/ios"
+python build_ios_libs.py --arch {arm64,arm} --bitcode --revision "${version}"
+cd "${build_dir}/src/out_ios_libs"
+timestamp=$(date +%s)
+zip -r "WebRTC_${timestamp}.zip" .
+mv WebRTC.framework.zip "${build_dir}"
---
+++ config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+branch="branch-heads/4044"
+version="81"
